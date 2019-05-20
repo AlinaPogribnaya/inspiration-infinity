59362f54afccfee18a96a70ee4de080eb52776a0
AlinaPogribnaya <48163920+AlinaPogribnaya@users.noreply.github.com>
2019-05-20 14:22:19 +0300

    Laba3 na Django

--- LabaDja3/Scripts/blog/articles/views.py
+++ LabaDja3/Scripts/blog/articles/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from .models import Article
 from django.shortcuts import render
-
+from django.http import Http404
 
 def archive(request):
     return render(request, 'archive.html', {"posts": Article.objects.all()})
--- LabaDja3/Scripts/blog/blog/urls.py
+++ LabaDja3/Scripts/blog/blog/urls.py
@@ -13,14 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
-from flatpages import views
+
 from django.contrib import admin
 from django.urls import path
 from articles import views
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.archive, name='archive'),
 ]
+url(
+r'^article/(?P<article_id>\d+)$',
+'articles.views.get_article',
+name='get_article'
+)
